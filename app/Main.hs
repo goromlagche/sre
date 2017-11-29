a6ba96ece76e38e066623f259167de6b0b4c07fe
goromlagche <mrinmoy.das91@gmail.com>
2017-11-29 18:51:49 +0530

    use aeson-lens for parsing json, now we dont have to define type

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,18 @@
 module Main where
 
 import Lib
-import DataSource
 import RuleParser
 import Data.Text.IO as TIO
 import Data.ByteString.Lazy as B
+import Control.Lens
+import Data.Aeson.Lens
 
 main :: IO ()
 main = do
-  wm <- parseEmployee <$> B.readFile "./ext/data_source.json"
+  ds <- B.readFile "./ext/data_source.json"
   rules <- parseRule <$> TIO.readFile "./ext/rules.txt"
   print "Working Memory"
-  print wm
+  print $ ds^..key "employees" . values.key "salary"
   print "Rules"
   print rules
   return ()
